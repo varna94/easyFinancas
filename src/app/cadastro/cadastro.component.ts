88dd978ef81a6ea887f4801fd578b3c41cdb1201
Silva <jessica.kelly.silva@accenture.com>
2020-12-12 17:25:54 -0300

    .

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  verifyEmail(email: any) {
+    if (email === '') {
+      console.log('email vazio-- ' + JSON.stringify(email.target.value));
+    } else {
+      console.log('email preenchido' + JSON.stringify(email));
+    }
   }
-
 }
